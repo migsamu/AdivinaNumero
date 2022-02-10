a13f251f197f2304f51e79216d9a257195f59245
Miguel <mufrikiler@gmail.com>
2022-02-10 21:18:11 +0100

    diseño de la segunda pantalla

---
+++ app/src/main/java/org/iesfm/adivinarnumero/Estadisticas.java
@@ -0,0 +1,14 @@
+package org.iesfm.adivinarnumero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Estadisticas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_estadisticas);
+    }
+}
