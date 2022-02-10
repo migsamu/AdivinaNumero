d5b3ea726b94b6eae6779b79f470c9315d863801
Miguel <mufrikiler@gmail.com>
2022-02-11 00:40:58 +0100

    no me funciona

--- app/src/main/java/org/iesfm/adivinarnumero/Estadisticas.java
+++ app/src/main/java/org/iesfm/adivinarnumero/Estadisticas.java
@@ -2,13 +2,50 @@ package org.iesfm.adivinarnumero;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class Estadisticas extends AppCompatActivity {
+
+    private TextView tvAciertos;
+    private TextView tvIntentos;
+    private TextView tvPorcentajeAciertos;
+    private TextView tvMediaIntentos;
+
+    private int aciertos;
+    private int intentos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estadisticas);
+        init();
+
+    }
+
+    public void init() {
+        tvAciertos = (TextView) findViewById(R.id.e_tvAciertos);
+        tvIntentos = (TextView) findViewById(R.id.e_tvIntentos);
+        tvPorcentajeAciertos = (TextView) findViewById(R.id.e_tvPorcentajeAciertos);
+        tvMediaIntentos = (TextView) findViewById(R.id.e_tvMediaIntentos);
+        getInfo();
+    }
+
+    public void getInfo() {
+
+        aciertos = getIntent().getIntExtra(MainActivity.ACIERTOS, 0);
+        intentos = getIntent().getIntExtra(MainActivity.INTENTOS, 0);
+
+        String aciertosStr = String.valueOf(aciertos);
+        String intentosStr = String.valueOf(intentos);
+
+        tvAciertos.setText(aciertosStr);
+        tvIntentos.setText(intentosStr);
     }
 }
--- app/src/main/java/org/iesfm/adivinarnumero/MainActivity.java
+++ app/src/main/java/org/iesfm/adivinarnumero/MainActivity.java
@@ -1,10 +1,14 @@
 package org.iesfm.adivinarnumero;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +17,9 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected final static String ACIERTOS = "org.iesfm.adivinarnumero.MainActivity";
+    protected final static String INTENTOS = "org.iesfm.adivinarnumero.MainActivity";
 
     private int numeroIntentos;
     private int numeroAciertos;
@@ -29,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(Menu.NONE, 1, Menu.NONE, getString(R.string.estadisticas));
+        return true;
+    }
+
 
     public void init() {
         etNumeroIntrodcucido = (EditText) findViewById(R.id.etuNumeroIntroducido);
@@ -57,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
             numeroIntentos++;
         }
         editor.putInt(getString(R.string.pref_aciertos), numeroAciertos);
-        editor.putInt(getString(R.string.pref_errores), numeroIntentos);
+        editor.putInt(getString(R.string.pref_intentos), numeroIntentos);
         editor.commit();
         etNumeroIntrodcucido.setText("");
     }
@@ -68,6 +83,21 @@ public class MainActivity extends AppCompatActivity {
         editor.putInt(getString(R.string.pref_numero), numeroAleatorio);
         editor.commit();
         btnRetry.setVisibility(View.GONE);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == 1) {
+            Intent intent = new Intent(this, Estadisticas.class);
+            int aciertos = pref.getInt(getString(R.string.pref_aciertos), numeroAciertos);
+            int intentos = pref.getInt(getString(R.string.pref_intentos), numeroIntentos);
+            intent.putExtra(ACIERTOS, aciertos);
+            intent.putExtra(INTENTOS, intentos);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
 
